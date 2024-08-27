910c150fd8b382cad905491abe7ccc39adf9e135
nyantama0616 <g2124029@fun.ac.jp>
2024-08-27 23:27:13 +0900

    それぞれのmoduleのtesterをそれぞれのmoduleに入れた

--- card/tester.rb
+++ card/tester.rb
@@ -2,38 +2,40 @@ require 'gosu'
 require_relative 'presenter'
 require_relative '../config'
 
-class TestWindow < Gosu::Window
-  def initialize
-    super Config::WINDOW_WIDTH, Config::WINDOW_HEIGHT
+module Card
+  class TestWindow < Gosu::Window
+    def initialize
+      super Config::WINDOW_WIDTH, Config::WINDOW_HEIGHT
 
-    @card = Card::Presenter.new('A', 'hart')
-    @card.set_position(100, 100)
-  end
+      @card = Presenter.new('A', 'hart')
+      @card.set_position(100, 100)
+    end
 
-  def update
-    handle_key_press
-  end
+    def update
+      handle_key_press
+    end
 
-  def draw
-    @card.draw
-  end
+    def draw
+      @card.draw
+    end
 
-  private
+    private
 
-  def handle_key_press
-    if Gosu.button_down?(Gosu::KB_LEFT)
-      @card.turn_left
-    elsif Gosu.button_down?(Gosu::KB_RIGHT)
-      @card.turn_right
-    elsif Gosu.button_down?(Gosu::KB_UP)
-      @card.turn_fore
-    elsif Gosu.button_down?(Gosu::KB_S)
-      @card.resize_small
-    elsif Gosu.button_down?(Gosu::KB_M)
-      @card.resize_middle
+    def handle_key_press
+      if Gosu.button_down?(Gosu::KB_LEFT)
+        @card.turn_left
+      elsif Gosu.button_down?(Gosu::KB_RIGHT)
+        @card.turn_right
+      elsif Gosu.button_down?(Gosu::KB_UP)
+        @card.turn_fore
+      elsif Gosu.button_down?(Gosu::KB_S)
+        @card.resize_small
+      elsif Gosu.button_down?(Gosu::KB_M)
+        @card.resize_middle
+      end
     end
   end
-end
 
-window = TestWindow.new
-window.show
+  window = TestWindow.new
+  window.show
+end
--- flag/tester.rb
+++ flag/tester.rb
@@ -2,36 +2,38 @@ require 'gosu'
 require_relative 'presenter'
 require_relative '../config'
 
-class TestWindow < Gosu::Window
-  def initialize
-    super Config::WINDOW_WIDTH, Config::WINDOW_HEIGHT
+module Flag
+  class TestWindow < Gosu::Window
+    def initialize
+      super Config::WINDOW_WIDTH, Config::WINDOW_HEIGHT
 
-    @x = 100
-    @y = 100
-    @flag = Flag::Presenter.new
-    @flag.set_position(@x, @y)
-  end
-
-  def update
-    handle_key_press
-  end
-
-  def draw
-    @flag.draw
-  end
-
-  private
-
-  def handle_key_press
-    if Gosu.button_down?(Gosu::KB_LEFT)
-      @flag.set_position(@x - 100, @y)
-    elsif Gosu.button_down?(Gosu::KB_RIGHT)
-      @flag.set_position(@x + 100, @y)
-    elsif Gosu.button_down?(Gosu::KB_UP)
+      @x = 100
+      @y = 100
+      @flag = Presenter.new
       @flag.set_position(@x, @y)
     end
-  end
-end
 
-window = TestWindow.new
-window.show
+    def update
+      handle_key_press
+    end
+
+    def draw
+      @flag.draw
+    end
+
+    private
+
+    def handle_key_press
+      if Gosu.button_down?(Gosu::KB_LEFT)
+        @flag.set_position(@x - 100, @y)
+      elsif Gosu.button_down?(Gosu::KB_RIGHT)
+        @flag.set_position(@x + 100, @y)
+      elsif Gosu.button_down?(Gosu::KB_UP)
+        @flag.set_position(@x, @y)
+      end
+    end
+  end
+
+  window = TestWindow.new
+  window.show
+end
