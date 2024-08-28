845a26bc743fb062181525b1aa0a49f7e1e16c46
Nao_Omura <nao0117.08194@icloud.com>
2024-08-28 16:24:32 +0900

    画像のサイズ調整

--- card/tester.rb
+++ card/tester.rb
@@ -6,7 +6,7 @@ module Card
     def initialize
       super
 
-      @card = Presenter.new('6', 'diamonds')
+      @card = Presenter.new('2', 'diamonds')
       @card.set_position(100, 100)
     end
 
