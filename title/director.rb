8d2bc8ce40b391cee5db553ea19c19adefec5b31
canon-g-2 <s245024@matsu.shimane-u.ac.jp>
2024-08-28 11:43:22 +0900

    タイトル画像とフラッグ画像の追加

---
+++ title/director.rb
@@ -0,0 +1,32 @@
+require 'gosu'
+
+module RC2024suTeam6
+  module Title
+    # タイトルシーンの担当ディレクタークラス
+    class Director < DirectorBase
+      # コンストラクタ
+      def initialize
+        super
+        @bg_img = Gosu::Image.new("images/title.png", tileable: true)
+        @bgm = load_bgm("bgm1.mp3", 0.3)
+      end
+
+      # 1フレーム分の更新処理
+      def update(opt = {})
+        @bgm.play if @bgm && !@bgm.playing?
+        # スペースキー押下でルール説明へ切り替えを行う
+        if key_push?(Gosu::KB_SPACE)
+          transition(:game)
+          @bgm.stop if @bgm && @bgm.playing?
+        end
+      end
+
+      # 1フレーム分の描画処理
+      def draw
+        @bg_img.draw(0, 0, 0)
+        draw_text("Ruby合宿2024夏 Example Game", :center, 280, font: :title, color: :red)
+        draw_text("Push SPACE Key to start", :center, 350)
+      end
+    end
+  end
+end
